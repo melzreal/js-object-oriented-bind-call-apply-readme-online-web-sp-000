69b4cd939e7f7fe5aab6e17cdc915e2965037806
Melissa Real <melissa.real@hotmail.com>
2019-04-12 12:13:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
-let sally = { name: 'Sally' };
-
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
 }
-
-greet.call(sally, 'Terry', 'George');
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
