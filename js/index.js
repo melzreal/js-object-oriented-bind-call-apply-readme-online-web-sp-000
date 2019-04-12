c5f1dc16cf128dd77006cf59cc6fdcd6e30c43c6
Melissa Real <melissa.real@hotmail.com>
2019-04-12 11:45:44 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-// use this to copy code snippets or use your browser's console
-
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
 }
-
-greet(); // my name is , hi!
-
-let person = {
-    name: 'Bob',
-    greet: greet
-};
-
-person.greet(); // my name is Bob, hi!
