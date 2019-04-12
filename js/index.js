db5ac63211305170c15d020d30ac66b61ec3631d
Melissa Real <melissa.real@hotmail.com>
2019-04-12 11:46:13 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 function greet(customerOne, customerTwo) {
     console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
 }
+let sally = { name: 'Sally' };
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+greet.call(sally, 'Terry', 'George');
